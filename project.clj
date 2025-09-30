039dc79e129f70a2866066a732703a4ce1712c4f
Vanja Komadinovic <vanjakom@gmail.com>
2025-09-30 17:22:29 +0200

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject com.mungolab/clj-ai "0.1.0-SNAPSHOT"
+(defproject
+  com.mungolab/clj-ai
+  "0.1.0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/clojure "1.11.1"]
                  [com.mungolab/clj-common "0.3.3"]])
