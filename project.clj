a81dd17fc2b9cb500a938081f16ba4da74818a36
Vanja Komadinovic <vanjakom@gmail.com>
2025-10-30 00:05:25 +0100

    deploy clojars

--- project.clj
+++ project.clj
@@ -3,6 +3,10 @@
   "0.1.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [
+                        ["clojars" {
+                                    :url "https://clojars.org/repo"
+                                    :sign-releases false}]]  
   :dependencies [
                  [org.clojure/clojure "1.11.1"]
                  [com.mungolab/clj-common "0.3.3"]])
